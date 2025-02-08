b482d0ea8765a244007387dce0e4d431ff5a26af
msufiyanrizvi21 <msufiyanrizvi21@gmail.com>
2025-02-09 03:47:34 +0500

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+using Microsoft.AspNetCore.Mvc;
+using SpendSmart.Models;
+using System.Diagnostics;
+
+namespace SpendSmart.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly SpendSmartDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, SpendSmartDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Expenses()
+        {
+            var allExpenses = _context.Expenses.ToList();
+
+            var totalExpense = allExpenses.Sum(x => x.Value);
+
+            ViewBag.Expenses = totalExpense;
+
+            return View(allExpenses);
+        }        
+        
+        public IActionResult CreateEditExpense(int? id)
+        {
+            if(id != null)
+            {
+                //Editing -> Load an expense by Id
+                var expenseInDb = _context.Expenses.SingleOrDefault(expense => expense.Id == id);
+                return View(expenseInDb);
+            }
+
+            return View();
+        }
+
+        public IActionResult DeleteExpense(int id)
+        {
+            var expenseInDb = _context.Expenses.SingleOrDefault(expense => expense.Id == id);
+            _context.Expenses.Remove(expenseInDb);
+            _context.SaveChanges();
+            return RedirectToAction("Expenses");
+
+        }
+
+        public IActionResult CreateEditExpenseForm(Expense model)
+        {
+            if(model.Id == 0)
+            {
+                //Create
+                _context.Expenses.Add(model);
+
+            } 
+            else
+            {
+                //Update
+                _context.Expenses.Update(model);
+
+            }
+
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Expenses");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Expense.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpendSmart.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+
+        public decimal Value{ get; set; }
+
+        [Required]
+
+        public string? Description { get; set; }
+
+
+    }
+}
